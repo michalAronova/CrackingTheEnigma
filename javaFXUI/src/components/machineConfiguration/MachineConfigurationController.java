6e43b8b7f7d3f40fb64719c4ef7fc0eff8d714b8
MichalGolan <michalgolan0107@gmail.com>
2022-08-20 13:54:02 +0300

    added machine configuration component - fxml and controller

---
+++ javaFXUI/src/components/machineConfiguration/MachineConfigurationController.java
@@ -0,0 +1,52 @@
+package components.machineConfiguration;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class MachineConfigurationController {
+
+    @FXML
+    private BorderPane rotorDetailsBorderPane;
+
+    @FXML
+    private Label rotorNumberLabel1;
+
+    @FXML
+    private Label rotorLocation1;
+
+    @FXML
+    private Label rotorDistance1;
+
+    @FXML
+    private Label rotorNumberLabel2;
+
+    @FXML
+    private Label rotorLocation2;
+
+    @FXML
+    private Label rotorDistance2;
+
+    @FXML
+    private Label rotorNumberLabel3;
+
+    @FXML
+    private Label rotorLocation3;
+
+    @FXML
+    private Label rotorDistance3;
+
+    @FXML
+    private Label reflectorIDlabel;
+
+    @FXML
+    private VBox plugsVBox;
+
+    @FXML
+    private Label plug1;
+
+    @FXML
+    private Label plug2;
+
+}
